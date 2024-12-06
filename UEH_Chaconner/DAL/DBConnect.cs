c242435c6a3d2a586829ed60e57bd8b1dfcb4a72
QHoa036 <hoadinhtruong0306@gmail.com>
2024-12-06 20:28:31 +0700

    Initial proj

---
+++ UEH_Chaconner/DAL/DBConnect.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.SqlClient;
+
+namespace DAL
+{
+    public class DBConnect
+    {
+        public static SqlConnection Connect()
+        {
+            string strcon = @"";
+            SqlConnection conn = new SqlConnection(strcon);
+            return conn;
+        }
+    }
+}
