96a257b15a048706ca1e918f4c3183b5ff3dd434
thaonguyenbi <thaonguyen.31221023052@st.ueh.edu.vn>
2024-12-20 21:02:11 +0700

    add comments

--- DAL/DBConnect.cs
+++ DAL/DBConnect.cs
@@ -6,8 +6,10 @@ namespace DAL
 {
     public class DBConnect
     {
+        //Kết nối với cơ sở dữ liệu bằng connectionString từ App.config
         private readonly SqlConnection _connect = new SqlConnection(ConfigurationManager.ConnectionStrings[@"Local"].ConnectionString);
 
+        //Kiểm tra kết nối có đang đóng không. Nếu có, mở kết nối
         public DBConnect()
         {
             if (_connect.State == ConnectionState.Closed) _connect.Open();
@@ -16,9 +18,11 @@ namespace DAL
         // Load dữ liệu từ database
         public DataTable Load_Data(string sql)
         {
+            //Tạo lệnh SQL với thủ tục lưu trữ (Stored Procedure)
             var cmd = new SqlCommand(sql, _connect) { CommandType = CommandType.StoredProcedure };
             var adapter = new SqlDataAdapter(cmd);
             var dt = new DataTable();
+            //Đổ dữ liệu vào DataTable rồi trả về
             adapter.Fill(dt);
             return dt;
         }
@@ -26,7 +30,9 @@ namespace DAL
         // Load dữ liệu từ database với tham số
         public DataTable LoadDataWithParameter(string sql, string[] name, object[] values, int parameter)
         {
+            //Tạo lệnh SQL với thủ tục lưu trữ (Stored Procedure)
             var cmd = new SqlCommand(sql, _connect) { CommandType = CommandType.StoredProcedure };
+            //Thêm tham số vào lệnh SQL
             for (var i = 0; i < parameter; i++) cmd.Parameters.AddWithValue(name[i], values[i]);
             var adapter = new SqlDataAdapter(cmd);
             var dt = new DataTable();
@@ -37,22 +43,28 @@ namespace DAL
         // Thêm, sửa, xóa dữ liệu
         public int ExecuteData(string sql, string[] name, object[] values, int parameter)
         {
+            //Tạo lệnh SQL với thủ tục lưu trữ (Stored Procedure)
             var cmd = new SqlCommand(sql, _connect) { CommandType = CommandType.StoredProcedure };
             for (var i = 0; i < parameter; i++) cmd.Parameters.AddWithValue(name[i], values[i]);
+            //Trả về số hàng bị ảnh hưởng
             return cmd.ExecuteNonQuery();
         }
 
         // Trả về giá trị
         public int ReturnValueInt(string sql)
         {
+            //Tạo lệnh SQL với thủ tục lưu trữ (Stored Procedure)
             var cmd = new SqlCommand(sql, _connect) { CommandType = CommandType.StoredProcedure };
+            //Trả về giá trị đầu tiên trong hàng đầu tiên
             return int.Parse(cmd.ExecuteScalar().ToString());
         }
 
         // Trả về giá trị với tham số
         public int ReturnValueIntWithParameter(string sql, string[] name, object[] values, int parameter)
         {
+            //Tạo lệnh SQL với thủ tục lưu trữ (Stored Procedure)
             var cmd = new SqlCommand(sql, _connect) { CommandType = CommandType.StoredProcedure };
+            //Thêm tham số vào lệnh SQL
             for (var i = 0; i < parameter; i++) cmd.Parameters.AddWithValue(name[i], values[i]);
             return int.Parse(cmd.ExecuteScalar().ToString());
         }
